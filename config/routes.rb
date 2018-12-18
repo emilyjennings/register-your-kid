c6e79b82b4d1d316b47046854b9bfcf23ad124f7
github username <github email address>
2018-12-18 18:59:15 -0500

    adding secure password to parents

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   # get 'parent/new'
   # get 'parent/create'
   # get 'course/index'
-
+  #
   resources :parents do
     resources :teachers, only: [:index, :show]
-    resources :kids
+    resources :kids, only: [:show]
     resources :courses, only: [:index, :show]
   end
+
+  resources :courses
 
   root 'course/welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
