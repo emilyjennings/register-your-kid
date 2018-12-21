064737d2b159aa01acd406ece6955d42583b0dc0
github username <github email address>
2018-12-20 22:22:46 -0500

    scope methods

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,8 @@
 class Course < ApplicationRecord
   has_many :parent
   has_many :teacher
+  scope :earlychildhood, -> (age) { where(age < 5)}
+  scope :youth, -> (age) { where(age > 5 && age < 12)}
+  scope :teen, -> (age) { where(age > 12)}
+
 end
