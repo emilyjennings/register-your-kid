91d683763c6bd1235ddcfe59c7976ce2006d5cab
github username <github email address>
2019-01-05 12:41:52 -0500

    added scope method for infants

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,8 +1,6 @@
 class Kid < ApplicationRecord
   belongs_to :parent
   belongs_to :course, optional: true
+  scope :infant, -> { where(age: 0) }
 
-  # def self.teenager?
-  #   @kid.age > 12
-  # end
 end
