2de251daede11a814849e89a22ee196bd29aa2bd
github username <github email address>
2019-02-09 14:57:37 -0500

    changed over to pg and ready for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_05_010037) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.string "age_group"
